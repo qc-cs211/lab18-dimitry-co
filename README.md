cc3715490b74e5ff220f738f6f167d884b99a82b
dimitry don’t <>
2021-04-13 18:18:18 +0000

    2nd draft

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 [![Work in Repl.it](https://classroom.github.com/assets/work-in-replit-14baed9a392b3a25080506f3b7b6d57f295ec2978f6f33ec97e36a161684cbe9.svg)](https://classroom.github.com/online_ide?assignment_repo_id=4569281&assignment_repo_type=AssignmentRepo)
 # Lab 18 for Dr. Waxman's CS211: Practice Arrays As Pointers in C++
 
-## Name:  
+## Name: Dimitry Gallego
 
 ### How much time did it take you to complete this exercise?
+20 min
   
   
-### What challenges (if any) did you work through to finish this assignment?
+### What challenges (if any) did you work through to finish this assignment? using hex numbers and adding them
 
 
 ### Did you use any external resources? (Cite them below)
--- page36_arrays_as_pointers.cpp
+++ page36_arrays_as_pointers.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 int main( ) {
     int b[3][2];
-    cout<<sizeof(b)<<endl;
+    for(int i=0;i<3;i++)
+      for(int j=0;j<2;j++)
+        b[i][j] = i+j;
+    cout<<"Array size"<<sizeof(b)<<endl;//sizeof- # of bytes
+    cout << "integer Size: "<<sizeof(int)<<endl;
+    cout<<"Pointer size: " <<sizeof(b+0)<<endl;
+    cout<<sizeof(*(b+0))<<endl; // size of the first row
 
-    cout<<"Pointer size: " << sizeof(b+0)<<endl;
-    cout<<sizeof(*(b+0))<<endl;
+    for(int i=0;i<3;i++){
+      for(int j=0;j<2;j++)
+        cout<< *(*(b+i)+j)<<" ";
+      cout<< endl;
+    }
 
     cout<<" b is: "<<b<<endl;
-    cout<<" b+1 is: "<<b+1<<endl;
+    cout<<" b+1 is: "<<b+1<<endl;//adds 8 bc first row is 8 bytes(2 int elements)
 
     cout<<" &b is: "<<&b<<endl;
     cout<<" &b+1 is: "<<&b+1<<endl<<endl;
